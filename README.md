1ca1ab6e625b7eca671ef357cbd2f704fcefd534
arjantijms <arjan.tijms@gmail.com>
2018-04-03 18:40:33 +0200

    PAYARA-2601 Added copyrights and updated notice file, added readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# Eclipse Payara tools
+This is a fork of the Eclipse GlassFish Tools, tailored and improved to work with Payara.
+
+## Building
+
+To build type `ant` in the root of this project. This will cause Corundum to be downloaded and the feature to be build targeting Eclipse oxygen.2. 
+Archived update sites will be found in `[project root]/build/repository/features`
+
+
+
+
+GlassFish is a trademark of Oracle Corporation.
+Payara is a trademark of Payara Foundation.
+
--- plugins/org.eclipse.payara.tools.ui/src/org/eclipse/payara/tools/ui/serverview/dynamicnodes/NodeTypes.java
+++ plugins/org.eclipse.payara.tools.ui/src/org/eclipse/payara/tools/ui/serverview/dynamicnodes/NodeTypes.java
@@ -7,6 +7,15 @@
  * SPDX-License-Identifier: EPL-2.0
  ******************************************************************************/
 
+/******************************************************************************
+ * Copyright (c) 2018 Payara Foundation
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ * SPDX-License-Identifier: EPL-2.0
+ ******************************************************************************/
+
 package org.eclipse.payara.tools.ui.serverview.dynamicnodes;
 
 import java.util.HashMap;
