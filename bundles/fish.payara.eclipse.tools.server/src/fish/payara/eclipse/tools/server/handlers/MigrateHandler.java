2df08851438024a8c75e30ae2faf8a6634cd1844
luiseufrasio <luis.eufrasio@gmail.com>
2023-03-15 15:32:44 -0300

    FISH-6772 : letting user choose destination folder & checking OS

--- bundles/fish.payara.eclipse.tools.server/src/fish/payara/eclipse/tools/server/handlers/MigrateHandler.java
+++ bundles/fish.payara.eclipse.tools.server/src/fish/payara/eclipse/tools/server/handlers/MigrateHandler.java
@@ -15,6 +15,7 @@ import org.eclipse.core.runtime.IPath;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.console.ConsolePlugin;
@@ -25,14 +26,14 @@ import org.eclipse.ui.handlers.HandlerUtil;
 
 public class MigrateHandler extends AbstractHandler {
 	
-	 public static final String PAYARA_TRANSFORMER = "fish.payara.transformer";
-	 public static final String PAYARA_TRANSFORMER_MAVEN = "fish.payara.transformer.maven";
-	 public static final String PAYARA_TRANSFORMER_VERSION = "0.2.10";
+	public static final String PAYARA_TRANSFORMER = "fish.payara.transformer";
+	public static final String PAYARA_TRANSFORMER_MAVEN = "fish.payara.transformer.maven";
+	public static final String PAYARA_TRANSFORMER_VERSION = "0.2.10";
 
     @Override
     public Object execute(ExecutionEvent event) throws ExecutionException {
     	Shell shell = HandlerUtil.getActiveWorkbenchWindow(event).getShell();
-    	
+
     	IStructuredSelection selection = getSelection(event);
         if (selection != null && !selection.isEmpty()) {
             Object firstElement = selection.getFirstElement();
@@ -40,14 +41,31 @@ public class MigrateHandler extends AbstractHandler {
                 IResource resource = (IResource) firstElement;
                 IPath path = resource.getLocation();
                 if (path != null) {
-                    String folderPath = path.toOSString();
-                    MessageDialog.openInformation(shell, "Folder Path", folderPath);
-                    runMvnCommand(folderPath, folderPath + "-jakartaEE10");
+                    String srcPath = path.toOSString();
+                    String destinationPath = chooseDestinationPath(srcPath, resource.getName());
+					int exitCode = runMvnCommand(srcPath, destinationPath);
+		            if (exitCode == 0) {
+		                MessageDialog.openInformation(shell, "Success", "Project " + destinationPath + " created successfully.");
+		            } else {
+		                MessageDialog.openError(shell, "Error", "Maven command failed with exit code " + exitCode + ".");
+		            }
                 }
             }
         }
-    	
         return null;
+    }
+    
+    private String chooseDestinationPath(String srcPath, String projectName) {
+    	Shell shell = new Shell();
+    	DirectoryDialog dialog = new DirectoryDialog(shell);
+    	dialog.setText("Choose a destination Folder");
+    	dialog.setMessage("Please select a Directory:");
+    	dialog.setFilterPath(srcPath);
+    	String selectedDirectory = dialog.open();
+    	if (selectedDirectory != null) {
+    	    return selectedDirectory + "/" + projectName + "-jakartaee10";
+    	}
+    	return srcPath + "-jakartaee10";
     }
     
     private IStructuredSelection getSelection(ExecutionEvent event) {
@@ -58,20 +76,17 @@ public class MigrateHandler extends AbstractHandler {
         return null;
     }
     
-    private void runMvnCommand(String srcPath, String targetPath) {
+    private int runMvnCommand(String srcPath, String targetPath) {
     	Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
         
-    	// Create a new message console
         MessageConsole console = new MessageConsole("Maven Command Console", null);
         ConsolePlugin.getDefault().getConsoleManager().addConsoles(new IConsole[]{console});
         
-        // Get the console stream
         MessageConsoleStream consoleStream = console.newMessageStream();
         
-        // Run the Maven command
         try {
         	List<String> command = new ArrayList<>();
-        	command.add("mvn.cmd");
+        	command.add(getMvnCommand());
         	command.add("package");
         	command.add(getTransformCommand());
         	command.add("-DselectedSource=" + srcPath);
@@ -82,30 +97,31 @@ public class MigrateHandler extends AbstractHandler {
         	builder.redirectErrorStream(true);
         	Process process = builder.start();
             
-            // Display the output in the console
         	BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
         	String line = null;
             while ((line = reader.readLine()) != null) {
                 consoleStream.println(line);
             }
             
-            // Wait for the process to finish
-            int exitCode = process.waitFor();
-            
-            if (exitCode == 0) {
-                MessageDialog.openInformation(shell, "Success", "Maven command completed successfully.");
-            } else {
-                MessageDialog.openError(shell, "Error", "Maven command failed with exit code " + exitCode + ".");
-            }
-            
+            return process.waitFor();
         } catch (IOException | InterruptedException e) {
             MessageDialog.openError(shell, "Error", "An error occurred while running the Maven command: " + e.getMessage());
+            return -1;
         }
     }
-    
+
     private String getTransformCommand() {
         return String.format("%s:%s:%s:run",
                 PAYARA_TRANSFORMER, PAYARA_TRANSFORMER_MAVEN, PAYARA_TRANSFORMER_VERSION);
+    }
+    
+    private String getMvnCommand() {
+    	String osName = System.getProperty("os.name");
+    	if (osName.contains("Windows")) {
+    	    return "mvn.cmd";
+    	} else {
+    		return "mvn";
+    	}
     }
 
 }
